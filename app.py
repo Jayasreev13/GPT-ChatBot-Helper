a1797a3027f86daba0159d557dd609032ac59eef
Jayasree Varadarajan <85859476+Jayasreev13@users.noreply.github.com>
2023-07-29 17:17:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,11 @@ def chatbot():
         for message in messages:
             st.write(message["content"]) 
         st.audio(text_to_speech(system_response), format="audio/wav")
-
+# Streamlit Dashboard
+st.markdown("<h1 style='text-align: center; color: blue;'>I am Jay! Your AI VideoBot </h1>", unsafe_allow_html=True)
+#st.image("jay_standgif.gif", width = 200)
+st.title("AI Videobot using GPT-3")
+st.header(" Start your Conversation with Jay!")
 
 
 
